8ead703be283c717112c7dc33debf9aad7a0105c
Rahul <rahul.aggarwal1197@gmail.com>
2020-07-31 15:51:27 +0530

    Updates

---
+++ precache-manifest.a1eaaf9b102ed75c3cdf1fdaec662552.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "303ee0ce19eccddcbb5c9695d2fb3be1",
+    "url": "/Netflix-clone-website/index.html"
+  },
+  {
+    "revision": "2d5eb2eef3b6b044b243",
+    "url": "/Netflix-clone-website/static/css/main.6b5ea873.chunk.css"
+  },
+  {
+    "revision": "e294c677604c1a8ea173",
+    "url": "/Netflix-clone-website/static/js/2.0695be80.chunk.js"
+  },
+  {
+    "revision": "894f61387b383e6e2e0543fb120b4656",
+    "url": "/Netflix-clone-website/static/js/2.0695be80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d5eb2eef3b6b044b243",
+    "url": "/Netflix-clone-website/static/js/main.1a78d19d.chunk.js"
+  },
+  {
+    "revision": "9e4f3802e876292f258c",
+    "url": "/Netflix-clone-website/static/js/runtime-main.b3f293e2.js"
+  }
+]);
